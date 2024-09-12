4fc76992fafb2fe9b99cb8614f5ed5a240802669
TiaArvivolia <arvivoliatia@gmail.com>
2024-09-12 12:29:50 +0700

    praktikum 1

---
+++ Jobsheet-3/jobsheet3/bin/praktikum1.dart
@@ -0,0 +1,26 @@
+// void main() {
+//   String test = "test2";
+
+//   if (test == "test1") {
+//     print("Test1");
+//   } else if (test == "test2") {  // Harus 'else if' (huruf kecil)
+//     print("Test2");
+//   } else {  // Harus 'else' (huruf kecil)
+//     print("Something else");
+//   }
+
+//   if (test == "test2") print("Test2 again");
+
+// }
+
+void main() {
+  bool test = true; // Mengubah string ke boolean
+
+  if (test) {
+    print("Kebenaran");
+  } else {
+    print("Kebohongan");
+  }
+}
+
+
