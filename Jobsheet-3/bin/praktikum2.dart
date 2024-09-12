1e368544ec84e17cf858ffa97ca673272a231d1d
TiaArvivolia <arvivoliatia@gmail.com>
2024-09-12 12:36:02 +0700

    praktikum 2

---
+++ Jobsheet-3/bin/praktikum2.dart
@@ -0,0 +1,18 @@
+// void main() {
+//   int counter = 0; // Deklarasi variabel counter dengan nilai awal 0
+
+//   while (counter < 33) {
+//     print(counter);
+//     counter++;
+//   }
+// }
+
+void main() {
+  int counter = 0; // Deklarasi variabel counter dengan nilai awal 0
+  do {
+    print(counter);
+    counter++;
+  } while (counter < 77);
+}
+
+
