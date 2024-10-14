f5f2c8499606c223cf18ad7422e11afe1f58b9a5
TiaArvivolia <arvivoliatia@gmail.com>
2024-10-14 15:40:47 +0700

    recently updated

--- bismillah_flutter/android/app/src/main/kotlin/com/example/bismillah_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.bismillah_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- flutter_tia/android/app/src/main/kotlin/com/example/flutter_tia/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_tia
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
